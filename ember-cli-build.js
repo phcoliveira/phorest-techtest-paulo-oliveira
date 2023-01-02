9be9b9fa92a1b3683d3a9ebd02c87827e6657510
Paulo Oliveira <oliveira.phc@gmail.com>
2023-01-02 18:49:59 +0000

    feat: install and setup Tailwind CSS

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    postcssOptions: {
+      compile: {
+        cacheInclude: [/.*\.hbs$/, /.*\.css$/, /.*\.html/],
+        enabled: true,
+        includePaths: ['app'],
+        plugins: [require('tailwindcss'), require('autoprefixer')],
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{html,hbs}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
